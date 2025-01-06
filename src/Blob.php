96e99b0e991b939b90959025d78e8d7793b7459a
Barret Storck <113543095+barretstorck@users.noreply.github.com>
2025-01-06 12:18:31 -0600

    Added more tests, improved write function, added more Blob functions

--- src/Blob.php
+++ src/Blob.php
@@ -61,6 +61,15 @@ class Blob
     }
 
     /**
+     * Parses the Blob data's current line as a CSV into an array.
+     * https://www.php.net/manual/en/function.fgetcsv.php
+     */
+    public function getcsv(null|int $length = null, string $separator = ',', string $enclosure = '"', string $escape = '\\'): array
+    {
+        return fgetcsv($this->resource, $length, $separator, $enclosure, $escape);
+    }
+
+    /**
      *
      */
     public function getResource()
@@ -78,6 +87,15 @@ class Blob
     }
 
     /**
+     * Moves the Blob's data pointer to the end of the data.
+     */
+    public function end(): self
+    {
+        $this->seek(0, SEEK_END);
+        return $this;
+    }
+
+    /**
      * Check if the data pointer is at the end of the Blob
      * https://www.php.net/manual/en/function.feof.php
      */
@@ -87,12 +105,34 @@ class Blob
     }
 
     /**
+     * Formats a given array of values into a CSV formatted line and writes it
+     * to the Blob's data.
+     * https://www.php.net/manual/en/function.fputcsv.php
+     */
+    public function putcsv(array $fields, string $separator = ',', string $enclosure = '\"', string $escape = '\\', string $eol = "\n"): int
+    {
+        return fputcsv($this->resource, $fields, $separator, $enclosure, $escape, $eol);
+    }
+
+    /**
      * Read data from the Blob into a string.
      * https://www.php.net/manual/en/function.fread.php
      */
     public function read(null|int $length = null): string
     {
         return fread($this->resource, $length ?? $this->getChunkSize());
+    }
+
+    /**
+     * Returns the number of bytes remaining until the end of the Blob data.
+     */
+    public function remaining(): int
+    {
+        $originalPosition = $this->tell();
+        $this->end();
+        $endPosition = $this->tell();
+        $this->seek($originalPosition);
+        return $endPosition - $originalPosition;
     }
 
     /**
@@ -113,6 +153,18 @@ class Blob
     {
         fseek($this->resource, $offset, $whence);
         return $this;
+    }
+
+    /**
+     * Returns the total size of the Blob's data in bytes.
+     */
+    public function size(): int
+    {
+        $originalPosition = $this->tell();
+        $this->end();
+        $size = $this->tell();
+        $this->seek($originalPosition);
+        return $size;
     }
 
     /**
@@ -137,12 +189,37 @@ class Blob
      * Write string data into the Blob.
      * https://www.php.net/manual/en/function.fwrite.php
      */
-    public function write(string $input, null|int $length = null): int
+    public function write($input, null|int $length = null): int
     {
-        return fwrite($this->resource, $input, $length);
+        if (!is_null($length) && $length <= 0) {
+            throw new InvalidArgumentException('Blob::write() requires $length to be null or a positive integer');
+        }
+
+        if (is_string($input)) {
+            return fwrite($this->resource, $input, $length);
+        }
+
+        if ($input instanceof Blob) {
+            $input = $input->getResource();
+        }
+
+        if (is_resource($input)) {
+            $bytes = 0;
+            while (!feof($input) && (is_null($length) || $bytes < $length)) {
+                $bufferSize = $this->getChunkSize();
+                if (!is_null($length)) {
+                    $remainingBytes = $length - $bytes;
+                    $bufferSize = min($bufferSize, $remainingBytes);
+                }
+
+                $buffer = fread($input, $bufferSize);
+                $bytes += fwrite($this->resource, $buffer);
+            }
+            return $bytes;
+        }
+
+        throw new InvalidArgumentException('Blob::write() requires $input to be aa string, Blob, or resource type.');
     }
-
-
 
     /**
      * Split the Blob data into chunks.
--- tests/BlobTest.php
+++ tests/BlobTest.php
@@ -4,12 +4,16 @@ namespace BarretStorck\Blobby\Tests;
 
 use BarretStorck\Blobby\Blob;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 /**
  *
  */
 class BlobTest extends TestCase
 {
+    /**
+     *
+     */
     public function testConstructorWithoutParamters(): void
     {
         // Given
@@ -25,6 +29,9 @@ class BlobTest extends TestCase
         );
     }
 
+    /**
+     *
+     */
     public function testOpen(): void
     {
         // Given
@@ -38,6 +45,218 @@ class BlobTest extends TestCase
         $this->assertInstanceOf(
             expected: Blob::class,
             actual: $blob,
+        );
+    }
+
+    /**
+     *
+     */
+    public static function provideGetSetChunkSize(): array
+    {
+        return [
+            'null' => [
+                'given' => null,
+                'expect' => Blob::DEFAULT_CHUNK_SIZE,
+            ],
+            'One' => [
+                'given' => 1,
+                'expect' => 1,
+            ],
+            'Negative One' => [
+                'given' => -1,
+                'expect' => Blob::DEFAULT_CHUNK_SIZE,
+            ],
+        ];
+    }
+
+    /**
+     *
+     */
+    #[DataProvider('provideGetSetChunkSize')]
+    public function testGetSetChunkSize($given, $expect): void
+    {
+        // Given
+        $blob = new Blob();
+
+        // When
+        $blob->setChunkSize($given);
+        $actual = $blob->getChunkSize();
+
+        // Then
+        $this->assertEquals(
+            expected: $expect,
+            actual: $actual,
+        );
+    }
+
+    /**
+     *
+     */
+    public function testGetResource(): void
+    {
+        // Given
+        $resource = fopen('php://temp', 'r+');
+        $blob = new Blob($resource);
+
+        // When
+        $actual = $blob->getResource();
+
+        // Then
+        $this->assertSame(
+            expected: $resource,
+            actual: $actual,
+        );
+    }
+
+    /**
+     *
+     */
+    public function testClose(): void
+    {
+        // Given
+        $blob = new Blob();
+
+        // When
+        $returned = $blob->close();
+        $resource = $blob->getResource();
+
+        // Then
+        $this->assertTrue($returned);
+        $this->assertFalse(is_resource($resource));
+    }
+
+    /**
+     *
+     */
+    public function testEnd(): void
+    {
+        // Given
+        $resource = fopen('php://temp', 'r+');
+        $data = 'Hello world';
+        fwrite($resource, $data);
+        rewind($resource);
+        $blob = new Blob($resource);
+
+        // When
+        $blob->end();
+
+        // Then
+        $this->assertEquals(
+            expected: strlen($data),
+            actual: ftell($resource),
+        );
+    }
+
+    /**
+     *
+     */
+    public function testEofTrue(): void
+    {
+        // Given
+        $resource = fopen('php://temp', 'r+');
+        $data = 'Hello world';
+        fwrite($resource, $data);
+        $blob = new Blob($resource);
+        $blob->read(8192); // Read to guarentee we're past the last byte
+
+        // When
+        $actual = $blob->eof();
+
+        // Then
+        $this->assertTrue($actual);
+    }
+
+    /**
+     *
+     */
+    public function testEofFalse(): void
+    {
+        // Given
+        $resource = fopen('php://temp', 'r+');
+        $data = 'Hello world';
+        fwrite($resource, $data);
+        rewind($resource); // Rewind so there are bytes after the pointer
+        $blob = new Blob($resource);
+
+        // When
+        $actual = $blob->eof();
+
+        // Then
+        $this->assertFalse($actual);
+    }
+
+    /**
+     *
+     */
+    public function testRead(): void
+    {
+        // Given
+        $resource = fopen('php://temp', 'r+');
+        $data = 'Hello world';
+        fwrite($resource, $data);
+        rewind($resource);
+        $blob = new Blob($resource);
+
+        // When
+        $actual = $blob->read();
+
+        // Then
+        $this->assertEquals(
+            expected: $data,
+            actual: $actual,
+        );
+    }
+
+    /**
+     *
+     */
+    public function testWriteString(): void
+    {
+        // Given
+        $data = 'Hello world';
+        $blob = new Blob();
+
+        // When
+        $actual = $blob->write($data);
+
+        // Then
+        $this->assertEquals(
+            expected: strlen($data),
+            actual: $actual,
+        );
+
+
+        $this->assertEquals(
+            expected: $data,
+            actual: $blob->rewind()->read(),
+        );
+    }
+
+    /**
+     *
+     */
+    public function testWriteBlob(): void
+    {
+        // Given
+        $data = 'Hello world';
+        $blob1 = new Blob();
+        $blob1->write($data);
+        $blob1->rewind();
+        $blob2 = new Blob();
+
+        // When
+        $actual = $blob2->write($blob1);
+
+        // Then
+        $this->assertEquals(
+            expected: strlen($data),
+            actual: $actual,
+        );
+
+
+        $this->assertEquals(
+            expected: $data,
+            actual: $blob2->rewind()->read(),
         );
     }
 }
